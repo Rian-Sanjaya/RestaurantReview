955c484bc83a502b4d292468b224bb45a4d329f3
Rian <rian.sj@gmail.com>
2019-06-01 12:00:24 +0700

    keyboard scroll view, AsyncStorage, ActivityIndicator

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -3,28 +3,115 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  AsyncStorage
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 export default class AddReview extends Component {
+  state = {
+    name: '',
+    rating: 0,
+    comment: '',
+    submitting: false
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('reviewer_name').then( name => {
+      if(name !== null && name !== undefined) {
+        this.setState({ name })
+      }
+    })
+  }
+  
   close = () => {
     this.props.navigation.goBack()
   }
 
+  submitReview = () => {
+    this.setState({ submitting: true })
+
+    // if (this.state.name !== null && this.state.name !== undefined) {
+    //   AsyncStorage.setItem('reviewer_name', this.state.name)
+    // }
+
+    AsyncStorage.removeItem('reviewer_name')
+
+    setTimeout( () => {
+      console.log('Submit review ok');
+      this.setState({ submitting: false });
+      this.props.navigation.goBack();
+    }, 2000)
+  }
+
   render() {
     return (
-      <View style={styles.root}>
+      <KeyboardAwareScrollView style={{ flex: 1, backgroundColor: '#FFF' }}>
+        <View style={styles.root}>
         
-        <TouchableOpacity 
-          style={styles.button}
-          onPress={this.close}
-        >
-          <Icon name='close' size={30} color='#0066CC' />
-        </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={this.close}
+          >
+            <Icon name='close' size={30} color='#0066CC' />
+          </TouchableOpacity>
 
-        <Text style={styles.addReview}>Add Review</Text>
-      </View>
+          <Text style={styles.addReview}>Add Review</Text>
+
+          <TextInput 
+            style={styles.input}
+            placeholder='Name (optional)'
+            value={this.state.name}
+            onChangeText={name => this.setState({name})}
+          />
+
+          <Text style={styles.rating}>Your Rating</Text>
+          <View style={styles.stars}>
+            {
+              [1, 2, 3, 4, 5].map( i => {
+                return <TouchableOpacity 
+                  onPress={() => this.setState({ rating: i })}
+                  style={styles.starButton}
+                  key={i}
+                >
+                  <Icon 
+                    name={'star'}
+                    color={this.state.rating >= i ? '#FFD64C' : '#CCCCCC'}
+                    size={40}
+                  />
+                </TouchableOpacity>
+              })
+            }
+          </View>
+
+          <TextInput 
+            style={[styles.input, { height: 100 }]}
+            placeholder='Review'
+            value={this.state.review}
+            onChangeText={review => this.setState({ review })}
+            multiline={true}
+            numberOfLines={5}
+          />
+
+          {
+            this.state.submitting && 
+            <ActivityIndicator size='large' color='#0066CC' style={{ padding: 10 }} />
+          }
+
+          <TouchableOpacity 
+            style={styles.submitButton}
+            onPress={this.submitReview}
+            disabled={this.state.submitting}
+          >
+            <Text style={styles.submitButtonText}>Submit Review</Text>
+          </TouchableOpacity>
+        
+        </View>
+      </KeyboardAwareScrollView>
     )
   }
 }
@@ -43,5 +130,39 @@ const styles = StyleSheet.create({
     color: '#444',
     textAlign: 'center',
     margin: 20
+  },
+  input: {
+    padding: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderRadius: 3
+  },
+  rating: {
+    fontSize: 20,
+    color: 'grey',
+    textAlign: 'center',
+    marginVertical: 40
+  },
+  stars: {
+    marginBottom: 80,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  starButton: {
+    padding: 5
+  },
+  submitButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#0066cc',
+    borderRadius: 4,
+    marginVertical: 10,
+    marginHorizontal: 20
+  },
+  submitButtonText: {
+    fontSize: 10,
+    color: '#ffffff',
+    textAlign: 'center'
   }
 })
